03f4a96093479a191bdbb849d5c906802fbbafb4
Ronlin1 <ronlinx6@gmail.com>
2021-12-01 15:42:56 +0300

    add test file

---
+++ test_pr.py
@@ -0,0 +1,21 @@
+# This is a test file containing test function code:
+# If you do not know Python: Just Add A comment by numbering them:
+# comment 1: Test 1 By Ronlin .....
+#
+#
+#
+
+def test_pr(code):
+    pr = list()
+    pr.insert(0, "Add Anything")
+
+    if code != " ":
+        pr.append(code)
+        print("Merged pull request, congs >>")
+        print(f"Main is now {pr}")
+    else:
+        print(f"Your empty '{code}' code has been rejected")
+your_pull_request = "food"
+test_pr(your_pull_request)
+
+# Just Add Anything in the function above if you know some python
